371ba9d7994e012034b78aff4363bff6364b5424
MantvydasSic <msiciovas@gmail.com>
2026-05-12 21:58:01 +0300

    pridetas zemelapis rodantis lokacija

--- frontend-mobile/app/build.gradle.kts
+++ frontend-mobile/app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
+    alias(libs.plugins.map.secret)
 }
 
 android {
@@ -39,6 +40,7 @@ dependencies {
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
+    implementation(libs.google.maps)
     implementation(libs.constraintlayout)
     // Fused Location Provider (single-shot high-accuracy requests)
     implementation("com.google.android.gms:play-services-location:21.0.1")
